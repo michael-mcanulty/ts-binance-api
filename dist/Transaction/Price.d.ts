38bcd6b8f8162106b4c01457d22fe831a50aa307
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-07-24 00:45:33 -0700

    Added getPrice to Price class

--- dist/Transaction/Price.d.ts
+++ dist/Transaction/Price.d.ts
@@ -1,5 +1,6 @@
 export declare class Price {
     symbol: string;
     value: number;
+    static GetPriceValue(prices: Price[], symbol: string): number;
     constructor(symbol: string, value: string | number);
 }
