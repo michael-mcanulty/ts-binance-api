d34b23672825b059a083c628895b8f9d7dc12b61
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-07-24 00:03:50 -0700

    Added getPrice to Price class

--- dist/Transaction/Price.js
+++ dist/Transaction/Price.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Price {
+    static GetPriceValue(prices, symbol) {
+        if (!prices || prices.length === 0)
+            return 0;
+        let priceFilter = prices.filter(x => x.symbol === symbol);
+        let price = 0;
+        if (priceFilter.length > 0) {
+            price = priceFilter[0].value;
+        }
+        return price;
+    }
     constructor(symbol, value) {
         this.symbol = symbol;
         if (value) {
--- src/Transaction/Price.ts
+++ src/Transaction/Price.ts
@@ -1,6 +1,16 @@
 export class Price {
 	symbol: string;
 	value: number;
+
+	public static GetPriceValue(prices: Price[], symbol: string): number {
+		if (!prices || prices.length === 0) return 0;
+		let priceFilter: Price[] = prices.filter(x => x.symbol === symbol);
+		let price: number = 0;
+		if (priceFilter.length > 0) {
+			price = priceFilter[0].value;
+		}
+		return price;
+	}
 	constructor(symbol: string, value: string | number) {
 		this.symbol = symbol;
 		if (value) {
