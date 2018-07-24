bd205291cc189a9f7fbe672bb2b35212c332b208
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-07-23 21:58:29 -0700

    Removed un-needed classes

--- src/Logger/BBLogger.ts
+++ src/Logger/BBLogger.ts
@@ -2,13 +2,13 @@ import * as fs from "fs";
 import {WriteStream} from "fs";
 import * as path from "path";
 
-export class Logger {
-	private static _INSTANCE: Logger;
+export class BBLogger {
+	private static _INSTANCE: BBLogger;
 	private static _base: string = path.dirname(process.cwd());
-	private static _dirBase: string = `${Logger._base}/${Logger._getAppName()}/logs/`;
-	private static _errorStreamPath: string = `${Logger._dirBase}/error.txt`;
-	private static _infoStreamPath: string = `${Logger._dirBase}/info.txt`;
-	private static _warningStreamPath: string = `${Logger._dirBase}/warning.txt`;
+	private static _dirBase: string = `${BBLogger._base}/${BBLogger._getAppName()}/logs/`;
+	private static _errorStreamPath: string = `${BBLogger._dirBase}/error.txt`;
+	private static _infoStreamPath: string = `${BBLogger._dirBase}/info.txt`;
+	private static _warningStreamPath: string = `${BBLogger._dirBase}/warning.txt`;
 
 	public static get Instance() {
 		return this._INSTANCE || (this._INSTANCE = new this());
@@ -17,7 +17,7 @@ export class Logger {
 	private static _getAppName(): string {
 		let appName: string = "";
 		let pathArr: string[] = process.cwd().split(path.sep);
-		let idxContains: number = Logger.indexContains(pathArr, "bb-");
+		let idxContains: number = BBLogger.indexContains(pathArr, "bb-");
 		if (idxContains >= 0) {
 			appName = pathArr[idxContains];
 		}
@@ -25,12 +25,12 @@ export class Logger {
 	}
 
 	private static _getMsg(msg: string): string {
-		return `${Logger.utcToPST(new Date()).toISOString()} at ${Logger._getAppName()} \n ${msg} \r\n`;
+		return `${BBLogger.utcToPST(new Date()).toISOString()} at ${BBLogger._getAppName()} \n ${msg} \r\n`;
 	}
 
 	public static error(msg: string) {
-		let logStream: WriteStream = fs.createWriteStream(Logger._errorStreamPath, {flags: 'a'});
-		logStream.write(Logger._getMsg(msg), () => {
+		let logStream: WriteStream = fs.createWriteStream(BBLogger._errorStreamPath, {flags: 'a'});
+		logStream.write(BBLogger._getMsg(msg), () => {
 			logStream.close();
 		});
 	}
@@ -45,8 +45,8 @@ export class Logger {
 	}
 
 	public static info(msg: string) {
-		let logStream: WriteStream = fs.createWriteStream(Logger._infoStreamPath, {flags: 'a'});
-		logStream.write(Logger._getMsg(msg), () => {
+		let logStream: WriteStream = fs.createWriteStream(BBLogger._infoStreamPath, {flags: 'a'});
+		logStream.write(BBLogger._getMsg(msg), () => {
 			logStream.close();
 		});
 	}
@@ -57,8 +57,8 @@ export class Logger {
 	}
 
 	public static warning(msg: string) {
-		let logStream: WriteStream = fs.createWriteStream(Logger._warningStreamPath, {flags: 'a'});
-		logStream.write(Logger._getMsg(msg), () => {
+		let logStream: WriteStream = fs.createWriteStream(BBLogger._warningStreamPath, {flags: 'a'});
+		logStream.write(BBLogger._getMsg(msg), () => {
 			logStream.close();
 		});
 	}
