8426a0602208c4887e98d00aaff71f87b2dd4c30
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-07-23 19:28:08 -0700

    Added type declarations files

--- dist/Transaction/Interfaces/IOrder.d.ts
+++ dist/Transaction/Interfaces/IOrder.d.ts
@@ -1,9 +1,7 @@
-import { IFill } from "./IFill";
 import { IBaseOrder } from "./IBaseOrder";
 export interface IOrder extends IBaseOrder {
     clientOrderId: string;
     executedQty: string;
-    fills?: IFill[];
     isWorking?: boolean;
     orderId: number;
     origQty: string;
