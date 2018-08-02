b1c1c8eac0afeae9ef371050ed51c90e08e29735
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-08-01 21:12:25 -0700

    # Added depositAddress, depositHistory, withdrawlHistory, and systemStatus methods in the Rest class

---
+++ dist/Withdraw/Interfaces/IWithdrawHistoryResult.d.ts
@@ -0,0 +1,15 @@
+import { EWithdrawStatus } from "../EWithdrawStatus";
+export interface WithdrawList {
+    id: string;
+    amount: number;
+    address: string;
+    asset: string;
+    txId: string;
+    applyTime: any;
+    status: EWithdrawStatus;
+    addressTag: string;
+}
+export interface IWithdrawHistoryResult {
+    withdrawList: WithdrawList[];
+    success: boolean;
+}
