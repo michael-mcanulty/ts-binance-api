3a2c2d02e343f0ce113fb97e1e76b80830f16774
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-08-01 17:03:17 -0700

    Merge branch 'master' of https://github.com/michael-mcanulty/ts-binance-api
    # Conflicts:
    #	tsconfig.json

---
+++ src/Balances/Interfaces/IAvailBalance.ts
@@ -0,0 +1,5 @@
+export interface IAvailBalance {
+	symbol:string;
+	available: number;
+	quoteAsset: string;
+}
---
+++ src/Balances/Interfaces/ITotalBalance.ts
@@ -0,0 +1,4 @@
+export interface ITotalBalance {
+	quoteAsset: string;
+	totalVal: number;
+}
