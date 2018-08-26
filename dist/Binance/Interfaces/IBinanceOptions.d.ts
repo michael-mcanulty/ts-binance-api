e93ed7ebf4b9b9e7e6c2b2b14c9c652928d6af40
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-08-26 01:33:04 -0700

    Automated Commit

--- dist/Binance/Interfaces/IBinanceOptions.d.ts
+++ dist/Binance/Interfaces/IBinanceOptions.d.ts
@@ -1,5 +1,6 @@
 import { IBinanceApiAuth } from "../../Account/Interfaces/IBinanceApiAuth";
 export interface IBinanceOptions {
+    _id?: string;
     auth: IBinanceApiAuth;
     test: boolean;
     useServerTime: boolean;
--- src/Binance/Interfaces/IBinanceOptions.ts
+++ src/Binance/Interfaces/IBinanceOptions.ts
@@ -1,6 +1,7 @@
 import {IBinanceApiAuth} from "../../Account/Interfaces/IBinanceApiAuth";
 
 export interface IBinanceOptions {
+	_id?: string;
 	auth: IBinanceApiAuth;
 	test: boolean;
 	useServerTime: boolean;
