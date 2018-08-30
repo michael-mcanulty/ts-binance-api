43d43603387456662c6a6bf2e775aa78b51c0327
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-08-29 21:38:47 -0700

    Automated Commit via binance-bot-cli

--- dist/Binance/Interfaces/IBinanceOptions.d.ts
+++ dist/Binance/Interfaces/IBinanceOptions.d.ts
@@ -1,7 +1,10 @@
 import { IBinanceApiAuth } from "../../Account/Interfaces/IBinanceApiAuth";
+import { IServiceOptions } from "../../Error/Email/Interfaces/IServiceOptions";
 export interface IBinanceOptions {
     _id?: string;
     auth: IBinanceApiAuth;
+    emailOptions: IServiceOptions;
+    errorMsgRecipient: string;
     test: boolean;
     useServerTime: boolean;
 }
--- src/Binance/Interfaces/IBinanceOptions.ts
+++ src/Binance/Interfaces/IBinanceOptions.ts
@@ -1,8 +1,11 @@
 import {IBinanceApiAuth} from "../../Account/Interfaces/IBinanceApiAuth";
+import {IServiceOptions} from "../../Error/Email/Interfaces/IServiceOptions";
 
 export interface IBinanceOptions {
 	_id?: string;
 	auth: IBinanceApiAuth;
+	emailOptions: IServiceOptions;
+	errorMsgRecipient: string;
 	test: boolean;
 	useServerTime: boolean;
 }
