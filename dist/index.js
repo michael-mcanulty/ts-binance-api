b6f5c94f3fcd6c43d02763b8bf22c1c0fef1aa1b
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-09-30 00:46:08 -0700

    #Fixing errors in the MarketBuy/MarketSell methods and Order classes.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var Fill_1 = require("./Transaction/Fill");
+exports.Fill = Fill_1.Fill;
 var TextMessage_1 = require("./TextMessage/TextMessage");
 exports.TextMessage = TextMessage_1.TextMessage;
 var AccountInfoOptions_1 = require("./Account/AccountInfoOptions");
