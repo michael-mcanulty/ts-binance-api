31aeb20f2295aa47daf5276b3d37e537b41c370c
Michael McAnulty <michael.mcanulty88@gmail.com>
2018-10-03 16:35:58 -0700

    Automated Commit via binance-bot-cli

--- src/Rest/BotHttp.ts
+++ src/Rest/BotHttp.ts
@@ -45,7 +45,7 @@ export class BotHttp {
 		let error: HttpError;
 		let res: Response;
 		try{
-			res = await requestPromise.get(uriOptions.uri.toString(), uriOptions);
+			res = await requestPromise.get(uriOptions);
 			json = await res.toJSON();
 			if (res.statusCode !== 200) {
 				error = new HttpError(res.statusCode, res.statusMessage);
