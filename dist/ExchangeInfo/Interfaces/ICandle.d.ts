85f7dbd36a9a4a73d4f26e4c8a08146f43e99513
Michael McAnulty <michael.mcanulty88@gmail.com>
2019-10-14 22:30:22 -0700

    Automated Commit via binance-bot-cli

--- dist/ExchangeInfo/Interfaces/ICandle.d.ts
+++ dist/ExchangeInfo/Interfaces/ICandle.d.ts
@@ -1,9 +1,11 @@
 export interface ICandle {
     closeTime: Date;
     openTime: Date;
+    open: number;
     high: number;
     interval?: string;
     low: number;
+    close: number;
     symbol?: string;
     volume: number;
 }
